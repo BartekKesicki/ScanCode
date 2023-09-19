70369be7b3d23401865858c66282d8e449dfa739
BartekK <bartek.kesicki25@gmail.com>
2023-09-20 00:22:41 +0200

    added simple qr code scanner with url launcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,28 +25,94 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  TextEditingController scannedCode = TextEditingController(text: "QR Code");
+  QRViewController? controller;
 
   @override
   Widget build(BuildContext context) {
+    var scanArea = 300.0;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("SCAN CODE"),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Column(
           children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: Stack(
+                children: [
+                  QRView(
+                    key: qrKey,
+                    overlay: QrScannerOverlayShape(
+                        borderColor: Colors.red,
+                        borderRadius: 10,
+                        borderLength: 40,
+                        borderWidth: 10,
+                        cutOutSize: scanArea),
+                    onQRViewCreated: _onQRViewCreated,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) async {
+      controller.pauseCamera();
+      await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("QR code decoded"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text('Do you want go to site: ${scanData.code}'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: const Text('GO!'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _launchUrl(scanData.code ?? "");
+                },
+              ),
+            ],
+          );
+        },
+      );
+      controller.resumeCamera();
+    });
+  }
+
+  void _launchUrl(String url) async {
+    final Uri uri = Uri.parse(url);
+    try {
+      await launchUrl(uri);
+    } catch(ex) {
+      print(ex);
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
